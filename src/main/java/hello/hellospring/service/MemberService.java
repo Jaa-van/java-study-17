a1da80b87b09be174d239efe992fc958dccbef9e
Jaa-van <finestecher1@gmail.com>
2024-01-02 21:00:15 +0900

    Feat Get MemberList api

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,9 +3,11 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
@@ -34,5 +36,9 @@ public class MemberService {
                         .ifPresent(m-> {
                             throw new IllegalStateException("이미 존재하는 회원입니다.");
                         });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
     }
 }
